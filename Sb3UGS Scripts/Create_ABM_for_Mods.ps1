18225303ec2624f1dc4f7b439d313cd020d85d7e
enimaroah-cubic <104311725+enimaroah-cubic@users.noreply.github.com>
2025-11-23 13:54:11 +0100

    Update Create_ABM_for_Mods.ps1
    Support for AI

--- Sb3UGS Scripts/Create_ABM_for_Mods.ps1
+++ Sb3UGS Scripts/Create_ABM_for_Mods.ps1
@@ -23,11 +23,13 @@ History:
 #>
 
 cd (Split-Path -Path $PSCommandPath)
+$ac = Test-Path -Path lib\ac_lib000_00
 $sv = Test-Path -Path abdata\sv_abdata
 $hc = Test-Path -Path abdata\abdata
-If (!$sv -and !$hc)
+$game = $(If ($ac) { "AC" } ElseIf ($sv) { "SV" } ElseIf ($hc) { "HC" })
+If (!$game)
 {
-	'We are neiter in HC nor in SV.'
+	'We are neiter in HC nor in SV nor in AC.'
 	Exit
 }
 $WshShell = New-Object -COMObject WScript.Shell
@@ -52,13 +54,14 @@ function Get-CABString
 
 $clipboard = Get-Clipboard
 
-$registered = Get-ChildItem -Path abdata\sv_a*, abdata\a*, abdata\craft* -Exclude sv_add007_45-MODS_WITHOUT_ABM, add007_45-MODS_WITHOUT_ABM -File -Force | 
+Write-Host -NoNewLine 'Reading ABMs '
+$registered = Get-ChildItem -Path abdata\sv_a*, abdata\a*, abdata\craft*, lib\ac_lib*, lib\craft* -Exclude ac_lib007_45-MODS_WITHOUT_ABM, sv_add007_45-MODS_WITHOUT_ABM, add007_45-MODS_WITHOUT_ABM -File -Force -ErrorAction SilentlyContinue | 
 	ForEach-Object {
-		$l = $_.FullName.LastIndexOf('\abdata\') + 1
+		Write-Host -NoNewLine '.'
 		@"
 LoadPlugin(PluginDirectory+"UnityPlugin.dll")
 unityParser = OpenUnity3d(path="
-"@ + $_.FullName.Substring($l, $_.FullName.Length - $l) + 
+"@ + ($_ | Resolve-Path -Relative) + 
 			@"
 ")
 unityEditor = Unity3dEditor(parser=unityParser)
@@ -67,22 +70,36 @@ unityEditor.CopyToClipboardAssetBundleManifest(asset=unityParser.Cabinet.Compone
 		cmd /c ".\SB3UtilityScript.lnk ""%CD%\ABM_Into_Clipboard.script"""
 		Get-Clipboard | Select-String -Pattern '<[^>]*>' -AllMatches | ForEach-Object { $_.Matches[1].Value.Substring(1, $_.Matches[1].Value.Length - 2) }
 	} | Sort-Object
+Write-Host
 Remove-Item ABM_Into_Clipboard.script
 
-$ab_set = Get-ChildItem -Path abdata\*.unity3d -Recurse -Force |
+$ab_set = Get-ChildItem -Path abdata\*.unity3d, lib\*.unity3d  -Recurse -Force -ErrorAction SilentlyContinue |
 	ForEach-Object {
-		$l = $_.FullName.LastIndexOf('\abdata\') + 8
-		$_.FullName.Substring($l, $_.FullName.Length - $l).Replace([IO.Path]::DirectorySeparatorChar, [IO.Path]::AltDirectorySeparatorChar)
+		Write-Host -NoNewLine "`rScanning for ABs ..." $_.Name "                    "
+		$relPath = ($_ | Resolve-Path -Relative)
+		$a = $relPath.Split('\')
+		$withoutBase = [String]::Empty
+		for ($i = 2; $i -lt $a.Length; $i++)
+		{
+			if ($i -gt 2)
+			{
+				$withoutBase += [IO.Path]::AltDirectorySeparatorChar
+			}
+			$withoutBase += $a[$i]
+		}
+		$withoutBase
 	} | Sort-Object
+Write-Host
 
 # https://stackoverflow.com/questions/53361968/powershell-compare-object-sideindicator
 
 $i = 745
 Compare-Object -ReferenceObject $registered -DifferenceObject $ab_set |
 	Where-Object { $_.SideIndicator -eq '=>' -and ($_.InputObject -NotLike 'sound/data/*' `
+											-and ($_.InputObject -NotLike 'sound/r/*' `
 											-and ($_.InputObject -NotLike 'craft/*' `
 											-and ($_.InputObject -NotLike 'localization/*' `
-											-and $_.InputObject -NotLike 'sprite/*'))) } |
+											-and $_.InputObject -NotLike 'sprite/*')))) } |
 	ForEach-Object {
 		$ret = "<$i><" + $_.InputObject + '><00000000000000000000000000000000>'
 		$i++
@@ -90,22 +107,33 @@ Compare-Object -ReferenceObject $registered -DifferenceObject $ab_set |
 	} |
 	Set-Clipboard
 
-$dest = $(If ($sv) {'sv_'} Else {''}) + 'add007_45-MODS_WITHOUT_ABM'
+$dest = $(switch ($game)
+	{
+		"AC" { 'ac_lib007_45' }
+		"SV" { 'sv_add007_45' }
+		"HC" { 'add007_45' }
+	}) + '-MODS_WITHOUT_ABM'
 If ($i -eq 745)
 {
 	'All ABs were already registered. No need to create an extra ABM for character mods.'
-	If ((Test-Path -Path ('abdata\' + $dest)))
+	If ((Test-Path -Path ($(If ($game -eq "AC") { 'lib\' } Else { 'abdata\' }) + $dest)))
 	{
-		Remove-Item -Path ('abdata\' + $dest)
+		Remove-Item -Path ($(If ($game -eq "AC") { 'lib\' } Else { 'abdata\' }) + $dest)
 	}
 }
 Else
 {
 	$cab = Get-CABString($dest)
+	$source = $(switch ($game)
+	{
+		"AC" { 'lib\ac_lib000_00' }
+		"SV" { 'abdata\sv_abdata' }
+		"HC" { 'abdata\abdata' }
+	})
 	@"
 LoadPlugin(PluginDirectory+"UnityPlugin.dll")
-unityParser = OpenUnity3d(path="abdata\
-"@ + @(If ($sv) {'sv_'} Else {''}) + 'abdata' + 
+unityParser = OpenUnity3d(path=".\
+"@ + $source + 
 		@"
 ")
 unityEditor = Unity3dEditor(parser=unityParser)
@@ -114,8 +142,8 @@ unityEditor.RenameCabinet(cabinetIndex=0, name="
 "@ + $cab +
 		@"
 ")
-unityEditor.SaveUnity3d(path=".\abdata\
-"@ + $dest +
+unityEditor.SaveUnity3d(path=".\
+"@ + ($(If ($game -eq "AC") { 'lib\' } Else { 'abdata\' }) + $dest) +
 		'", keepBackup=False, backupExtension="", background=False, clearMainAsset=True, pathIDsMode=-1, compressionLevel=0, compressionBufferSize=0)' |
 		Set-Content Clipboard_Into_ABM.script
 	cmd /c ".\SB3UtilityScript.lnk ""%CD%\Clipboard_Into_ABM.script"""
@@ -137,22 +165,33 @@ Compare-Object -ReferenceObject $registered -DifferenceObject $ab_set |
 	} |
 	Set-Clipboard
 
-$dest = 'craft' + $(If ($sv) {'027'} Else {'007'}) + '_45-MODS_WITHOUT_ABM'
+$dest = 'craft' + $(switch ($game)
+	{
+		"AC" { '047_45' }
+		"SV" { '027_45' }
+		"HC" { '007_45' }
+	}) + '-MODS_WITHOUT_ABM'
 If ($i -eq 1745)
 {
 	'All DC related ABs were already registered. No need to create an extra ABM for DC mods.'
-	If ((Test-Path -Path ('abdata\' + $dest)))
+	If ((Test-Path -Path ($(If ($game -eq "AC") { 'lib\' } Else { 'abdata\' }) + $dest)))
 	{
-		Remove-Item -Path ('abdata\' + $dest)
+		Remove-Item -Path ($(If ($game -eq "AC") { 'lib\' } Else { 'abdata\' }) + $dest)
 	}
 }
 Else
 {
 	$cab = Get-CABString($dest)
+	$source = $(switch ($game)
+	{
+		"AC" { 'lib\ac_lib000_00' }
+		"SV" { 'abdata\sv_abdata' }
+		"HC" { 'abdata\abdata' }
+	})
 	@"
 LoadPlugin(PluginDirectory+"UnityPlugin.dll")
-unityParser = OpenUnity3d(path="abdata\
-"@ + @(If ($sv) {'sv_'} Else {''}) + 'abdata' + 
+unityParser = OpenUnity3d(path=".\
+"@ + $source + 
 		@"
 ")
 unityEditor = Unity3dEditor(parser=unityParser)
@@ -161,8 +200,8 @@ unityEditor.RenameCabinet(cabinetIndex=0, name="
 "@ + $cab +
 		@"
 ")
-unityEditor.SaveUnity3d(path=".\abdata\
-"@ + $dest +
+unityEditor.SaveUnity3d(path=".\
+"@ + ($(If ($game -eq "AC") { 'lib\' } Else { 'abdata\' }) + $dest) +
 		'", keepBackup=False, backupExtension="", background=False, clearMainAsset=True, pathIDsMode=-1, compressionLevel=0, compressionBufferSize=0)' |
 		Set-Content Clipboard_Into_ABM.script
 	cmd /c ".\SB3UtilityScript.lnk ""%CD%\Clipboard_Into_ABM.script"""
